df80bf7c6fa72ef32add7d08845860fb8517434b
Palaash Atri <40315170+palaashatri@users.noreply.github.com>
2023-10-25 23:24:48 +0530

    Add comments

--- scripts/hyper-v-on-windows-home.bat
+++ scripts/hyper-v-on-windows-home.bat
@@ -1,3 +1,11 @@
+@REM Simple batch script to download and install Hyper-V manager on devices running Windows 10/11 Home, 
+@REM so that you can run both Docker and a full-fledged, hardware-accelerated, virtualized OS alongwith Windows on this Type-0 hypervisor
+
+@REM How to run?
+@REM Open a conhost as administrator (in PowerShell, use "Start-Process cmd.exe -Verb runAs"), and then 
+@REM execute this script, simply by calling "hyper-v-on-windows-home.bat"
+
+
 pushd "%~dp0"
 dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
 for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
