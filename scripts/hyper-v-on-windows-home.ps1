432585d580675ced6eefe22a1a028b443689e892
Palaash Atri <40315170+palaashatri@users.noreply.github.com>
2023-10-27 19:14:50 +0530

    Update hyper-v-on-windows-home.ps1

--- scripts/hyper-v-on-windows-home.ps1
+++ scripts/hyper-v-on-windows-home.ps1
@@ -1,5 +1,5 @@
 # This script downloads and installs Hyper-V manager on devices running Windows 10/11 Home, 
-# so that you can run both Docker and a full-fledged, hardware-accelerated, virtualized OS along with Windows on this Type-0 hypervisor. 
+# so that you can run both Docker and a full-fledged, hardware-accelerated, virtualized OS along with Windows on this Type-1 hypervisor. 
 # To run this script, open a PowerShell console as administrator and execute this script by calling “hyper-v-on-windows-home.ps1”.
 
 pushd $PSScriptRoot
