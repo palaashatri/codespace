04196638e8c767cb6dc3008c8abd4fbb1c6e77f4
Palaash Atri <palaash.atri@gmail.com>
2023-11-05 15:31:51 +0530

    netbeans recommended this idk why

--- learning-stuff/java-activities/demo/src/main/java/com/example/Main.java
+++ learning-stuff/java-activities/demo/src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.logging.Logger;
+import static java.lang.System.*;
 
 public class Main {
     static Logger logger = Logger.getLogger("MyLogger");
@@ -12,54 +13,54 @@ public class Main {
 
         // byte
         byte b = 127;
-        System.out.println("Size of byte is : " + Byte.SIZE);
-        System.out.println("Byte goes from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
-        System.out.println("Value of b is " + b);
+        out.println("Size of byte is : " + Byte.SIZE);
+        out.println("Byte goes from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        out.println("Value of b is " + b);
 
         // short 
         short s = 127;
         // or
         short s2 = (short) 127;
-        System.out.println("Size of short is : " + Short.SIZE);
-        System.out.println("Short goes from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
-        System.out.println("Value of s is : " + s + " " + s2);
+        out.println("Size of short is : " + Short.SIZE);
+        out.println("Short goes from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        out.println("Value of s is : " + s + " " + s2);
 
         // int -> default data type for numbers in Java
         int i = 127;
-        System.out.println("Size of integer is : " + Integer.SIZE); 
-        System.out.println("Integer goes from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
-        System.out.println("Value of i is : " + i);
+        out.println("Size of integer is : " + Integer.SIZE); 
+        out.println("Integer goes from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        out.println("Value of i is : " + i);
 
         // long -> use L suffix for specifying long integers, like 10L for example.
         long l = 127L;
-        System.out.println("Size of long is : " + Long.SIZE);
-        System.out.println("Long goes from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
-        System.out.println("Value of l is : " + l);
+        out.println("Size of long is : " + Long.SIZE);
+        out.println("Long goes from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        out.println("Value of l is : " + l);
 
         // float -> Use suffix F for specifying float decimal numbers
         float f = 127.27F;
         // or
         float f2 = (float) 127.27;
-        System.out.println("Size of float is : " + Float.SIZE);
-        System.out.println("Float goes from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
-        System.out.println("Value of f is : " + f + " " + f2);
+        out.println("Size of float is : " + Float.SIZE);
+        out.println("Float goes from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        out.println("Value of f is : " + f + " " + f2);
 
         // double -> Use suffix D for specifying double decimal numbers (optional), default data type for decimal numbers in Java
         double d = 127.27;
-        System.out.println("Size of double is : " + Double.SIZE);
-        System.out.println("Double goes from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
-        System.out.println("Value of d is : " + d);
+        out.println("Size of double is : " + Double.SIZE);
+        out.println("Double goes from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        out.println("Value of d is : " + d);
 
         // char -> default data type for characters in Java
         char c = 'D';
         char c1 = '\u0044'; // -> D
         char c2 = 68; // -> D
-        System.out.println("Size of char is : " + Character.SIZE);
-        System.out.println("Char goes from " + Character.MIN_VALUE + " to " + Character.MAX_VALUE);
-        System.out.println("Value of c is : " +  c + " " +  c1 + " " + c2);
+        out.println("Size of char is : " + Character.SIZE);
+        out.println("Char goes from " + Character.MIN_VALUE + " to " + Character.MAX_VALUE);
+        out.println("Value of c is : " +  c + " " +  c1 + " " + c2);
 
         // boolean -> default data type for boolean values in Java
-        System.out.println("Boolean has two values : " + Boolean.TRUE + " and " + Boolean.FALSE);
+        out.println("Boolean has two values : " + Boolean.TRUE + " and " + Boolean.FALSE);
 
     }
 
@@ -69,7 +70,7 @@ public class Main {
      */
     private static void convertPoundsToKilograms(double weightInPound){
         double weightInKilograms = weightInPound * 0.45359237;
-        System.out.println("Weight in kilograms is : " + weightInKilograms);
+        out.println("Weight in kilograms is : " + weightInKilograms);
     }
 
     /**
@@ -78,7 +79,7 @@ public class Main {
      */
     private static void convertKilogramsToPounds(double weightInKilograms){
         double weightInPounds = weightInKilograms * 2.20462262185;
-        System.out.println("Weight in pounds is : " + weightInPounds);
+        out.println("Weight in pounds is : " + weightInPounds);
     
     }
 
@@ -90,18 +91,18 @@ public class Main {
         char mySimpleChar = '?';
         char myUnicodeChar = '\u003f' ;
         char myDecimalChar = 63;
-        System.out.println("My Values are : " + mySimpleChar + myUnicodeChar + myDecimalChar);
+        out.println("My Values are : " + mySimpleChar + myUnicodeChar + myDecimalChar);
     }
 
     private static void diffLossyConversion(){
         int result = 10;
         result -= 5.5;
-        System.out.println(result); // -> 4 (lossy)
+        out.println(result); // -> 4 (lossy)
 
         // similar to
         int result2 = 10;
         result2  = (int) (result2 - 5.5);
-        System.out.println(result2); // -> 4 (lossy)
+        out.println(result2); // -> 4 (lossy)
  
         // This proves that, compound assignment operator :
         // x -= y
@@ -114,7 +115,7 @@ public class Main {
 
         double result3 = 10;
         result3 -= 5.5;
-        System.out.println(result3); // -> 4.5 (lossless)
+        out.println(result3); // -> 4.5 (lossless)
     }
 
     public static void main(String[] args) {
